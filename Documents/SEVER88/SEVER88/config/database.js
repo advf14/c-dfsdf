604e0f03e0980c2f3a85814081ba0577fa94f4ac
advf14 <hoangquocthien65@gmail.com>
2025-12-20 22:03:18 +0700

    add allowed IP range

---
+++ Documents/SEVER88/SEVER88/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'url': 'mongodb+srv://advf133:hoangquocthien@cluster0.hyrzdvw.mongodb.net/?appName=Cluster0',
+	'options': {
+		'dbName': 'CLUB3333', // red
+		//'dbName': 'GAME', // red
+		//'dbName': 'admin', // red
+                //'dbName': 'vn11022021', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
