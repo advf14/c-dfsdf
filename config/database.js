e2bfdaf4ca9609f7ffc78074254d4cd8e16138d4
advf14 <hoangquocthien65@gmail.com>
2025-12-21 18:20:19 +0700

    c

--- config/database.js
+++ config/database.js
@@ -1,12 +1,15 @@
 module.exports = {
-	'url': 'mongodb+srv://advf133:hoangquocthien@cluster0.hyrzdvw.mongodb.net/?appName=Cluster0',
-	'options': {
-		'dbName': 'CLUB3333', // red
-		//'dbName': 'GAME', // red
-		//'dbName': 'admin', // red
-                //'dbName': 'vn11022021', // red
-		'useNewUrlParser': true,
-		'useUnifiedTopology': true,
-		//'autoIndex':       false,
-	},
+    'url': 'mongodb+srv://advf133:hoangquocthien@cluster0.hyrzdvw.mongodb.net/CLUB3333?retryWrites=true&w=majority',
+    'options': {
+        'useNewUrlParser': true,
+        'useUnifiedTopology': true,
+        'useCreateIndex': true,
+        'useFindAndModify': false,
+        'serverSelectionTimeoutMS': 5000,
+        'socketTimeoutMS': 45000,
+        'connectTimeoutMS': 10000,
+        'maxPoolSize': 100,
+        'minPoolSize': 5,
+        'maxIdleTimeMS': 30000
+    }
 };
