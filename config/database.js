1abc157e5cc691e8ab98d0aeebb75bf0afdc7382
advf14 <hoangquocthien65@gmail.com>
2025-12-23 17:55:19 +0700

    C

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': process.env.MONGODB_URI || 'mongodb+srv://hoangquocthien20_db_user:cU9iBfkCH5TWl3QI@cluster0c.lwu9sjb.mongodb.net/?appName=Cluster0c',
+	'options': {
+		//'user':   'kvcpro',
+		//'pass':   'Minhhoang',
+		'dbName': 'RongVang88fun', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
